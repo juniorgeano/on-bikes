333a4e7a66bfaf99f7083e61ce4875fc3cb5fa8d
juniorgeano <71074230+juniorgeano@users.noreply.github.com>
2020-11-22 18:26:09 -0300

    master

---
+++ inc/db.js
@@ -0,0 +1,13 @@
+// get the client
+const mysql = require('mysql2');
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'mysql741.umbler.com',
+  user: 'onbikes',
+  database: 'on-bikes',
+  password: '18061987Jr',
+  multipleStatements: true
+  });
+
+  module.exports = connection;
